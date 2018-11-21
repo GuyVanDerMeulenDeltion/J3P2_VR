ee8fa4e8d7c9a86cc8d440a6d328dc80790fe746
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2018-11-21 10:17:37 +0100

    Photon setup

---
+++ Y3P2_VR/Assets/Individual/Sebastiaan/BaiscMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaiscMove : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime*speed, 0, Input.GetAxis("Vertical")*Time.deltaTime*speed));
+    }
+}
