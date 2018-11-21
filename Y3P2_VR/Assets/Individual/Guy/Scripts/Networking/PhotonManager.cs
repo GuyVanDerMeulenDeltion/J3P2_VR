92ac67a54185659a2009ae4487af2261db517ad8
GuyvandermeulenDeltion <guy_meulen@live.nl>
2018-11-21 12:04:52 +0100

    Attempt

---
+++ Y3P2_VR/Assets/Individual/Guy/Scripts/Networking/PhotonManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviour {
+
+    public static PhotonManager photon;
+
+    [Header("Menu:")]
+    public GameObject menu;
+
+    private void Awake() {
+        if (photon != null) return;
+        photon = this;
+    }
+
+    public void Start() {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void OnConnectedToMaster() {
+        print("Connected to master.");
+    }
+
+    public void OnJoinedLobby() {
+        Debug.Log("Joined Lobby.");
+    }
+}
---
+++ Y3P2_VR/Assets/Individual/Guy/Scripts/Networking/PhotonUIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonUIManager : MonoBehaviour
+{
+    public static PhotonUIManager photonUI;
+
+    [Header("Player Settings:")]
+    public InputField name;
+
+    [Header("Create Room Settings:")]
+    public InputField createRoomField;
+
+    [Header("Create Room Settings:")]
+    public InputField joinRoomField;
+
+    [Header("Status UI:")]
+    public GameObject[] connectionStatus;
+
+    public void Awake() {
+        if (photonUI != null) return;
+        photonUI = this;
+    }
+
+    public void CreateRoom() {
+       // PhotonManager.photon.GoToOrCreateRoom(createRoomField.text);
+    }
+
+    //Loads up the correct UI that represents the current connection status;
+    public void SetVisualConnectionStatus(int _Index) {
+        foreach (GameObject _Status in connectionStatus)
+            _Status.SetActive(false);
+
+            connectionStatus[_Index].SetActive(true);
+    }
+}
