2fe6e0e9d7dfe1a404d13691a1dc584cc143b349
DominikBeens <SchoolPC>
2018-11-28 12:50:13 +0100

    interaction system start

---
+++ New project folder/Y3P2 VR/Assets/Individual/Sebastiaan/InteractionManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VR;
+using Valve.VR;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour
+{
+
+    private bool leftHandOccupied;
+    private bool rightHandOccupied;
+
+    public GameObject leftHandSpawnPos {  get {  return (transform.GetChild(0).gameObject); } }
+    public GameObject rightHandSpawnPos {  get { return (transform.GetChild(1).gameObject); } }
+
+    public float rightHandAxis { get { return SteamVR_Input._default.inActions.Squeeze.GetAxis(SteamVR_Input_Sources.RightHand); } }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "interactible")
+        {
+            if (rightHandAxis > 0.85f)
+            {
+
+            }
+        }
+    }
+}
