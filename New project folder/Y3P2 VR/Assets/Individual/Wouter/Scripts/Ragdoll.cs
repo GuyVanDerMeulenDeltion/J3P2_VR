022c967c4377c9570be184df486ae8d0cc157043
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2018-11-30 12:01:50 +0100

    als de physics niet gefixed zijn ga ik echt huilen

---
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/Ragdoll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour {
+
+    public bool doRagdoll;
+    private Animator myAnim;
+    private Rigidbody myRB;
+
+    private void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        myRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(doRagdoll)
+        {
+            myAnim.enabled = false;
+            myRB.constraints = RigidbodyConstraints.None;
+        }
+        else
+        {
+            myAnim.enabled = true;
+            myRB.constraints = RigidbodyConstraints.FreezeAll;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, 1f / 1 * Time.deltaTime * 2);
+            //transform.position = Vector3.Lerp(transform.position, Physics.Raycast( )
+        }
+    }
+}
