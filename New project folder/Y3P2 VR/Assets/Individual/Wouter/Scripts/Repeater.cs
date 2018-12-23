eaa2d56f008f821129273d008589f76286bf6762
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-12-23 21:10:07 +0100

    Repeater prefab

---
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/Repeater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repeater : MonoBehaviour {
+
+    public Animator myAnim;
+    private float triggerAxis;
+    private float rechamberAxis;
+
+    void Update()
+    {
+        triggerAxis = Mathf.Clamp(Input.GetAxis("Horizontal"), 0, 0.95f);
+        rechamberAxis = Mathf.Clamp(Input.GetAxis("Vertical"), 0, 0.95f);
+
+        myAnim.SetFloat("TriggerAxis", triggerAxis);
+        myAnim.SetFloat("RechamberAxis", rechamberAxis);
+    }
+}
