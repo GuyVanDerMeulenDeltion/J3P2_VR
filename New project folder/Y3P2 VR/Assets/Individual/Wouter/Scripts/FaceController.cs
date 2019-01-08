4c47e70839a82e481276f21f36c392d7dc23ab08
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2019-01-08 19:23:12 +0100

    New facerig tech, crab enemy, wip dances

--- New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/FaceController.cs
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/FaceController.cs
@@ -4,32 +4,87 @@ using UnityEngine;
 
 public class FaceController : MonoBehaviour {
 
-    public int currentEmotion;
+    public int currentEmotion = 1;
     public float timer = 0;
     private float multiplier = 1;
+    public bool hurt;
+    public bool dead;
+    public KekDoll kekdollScript;
 
-    
+    public GameObject obj1;
+    public GameObject obj2;
+    public GameObject obj3;
+    public GameObject obj4;
+    public GameObject obj5;
 
- 
-
-    void Update()
+    public void Start()
     {
-
-
+        UpdateFace();
+        kekdollScript = GetComponentInParent<KekDoll>();
     }
 
-    public void RandomFaces()
+    public void Update()
     {
-        if (timer <= 0)
+        hurt = kekdollScript.doRagdoll;
+        if(hurt)
         {
-            timer = 5;
-            multiplier = Random.Range(0.75f, 1.5f);
+            currentEmotion = 4;
+            timer = 1;
+            UpdateFace();
+        }
+        if (timer > 0)
+        {
+            if(!hurt)
+            {
+                timer -= Time.deltaTime;
+            }
+
+            
         }
         else
         {
-            timer -= Time.deltaTime * multiplier;
+            timer = 3;
+            UpdateFace();
+            currentEmotion = Random.Range(1, 3);
+        }
+        if(dead)
+        {
+            currentEmotion = 5;
+            obj1.SetActive(false);
+            obj2.SetActive(false);
+            obj3.SetActive(false);
+            obj4.SetActive(false);
+            obj5.SetActive(true);
         }
     }
 
+    public void UpdateFace()
+    {
+        obj1.SetActive(false);
+        obj2.SetActive(false);
+        obj3.SetActive(false);
+        obj4.SetActive(false);
+        obj5.SetActive(false);
 
+        if(currentEmotion == 1)
+        {
+            obj1.SetActive(true);
+        }
+        if (currentEmotion == 2)
+        {
+            obj2.SetActive(true);
+        }
+        if (currentEmotion == 3)
+        {
+            obj3.SetActive(true);
+        }
+        if (currentEmotion == 4)
+        {
+            obj4.SetActive(true);
+        }
+        if (currentEmotion == 5)
+        {
+            obj5.SetActive(true);
+        }
+    }
 }
