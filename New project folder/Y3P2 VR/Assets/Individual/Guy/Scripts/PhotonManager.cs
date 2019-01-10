20df3d0f46f656c1ac4ccbf184ce0e6772c1a6ca
GuyvandermeulenDeltion <guy_meulen@live.nl>
2019-01-10 11:52:17 +0100

    Server settings

--- New project folder/Y3P2 VR/Assets/Individual/Guy/Scripts/PhotonManager.cs
+++ New project folder/Y3P2 VR/Assets/Individual/Guy/Scripts/PhotonManager.cs
@@ -7,35 +7,30 @@ using Photon.Realtime;
 public class PhotonManager : MonoBehaviourPunCallbacks {
 
     public static PhotonManager photonManager;
-    public bool joinRandom = false;
 
-	// Use this for initialization
-	private void Awake () {
+    // Use this for initialization
+    private void Awake() {
         DontDestroyOnLoad(this);
 
         if (photonManager != null)
-        Destroy(this);
+            Destroy(this);
         photonManager = this;
-	}
+    }
 
     private void Start() {
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster() {
-        CreateRoom("Test");
-    }
-
-    public void CreateRoom(string _RoomID) {
-        print("Joined room");
-        RoomOptions _Options = new RoomOptions { IsVisible = true, IsOpen = true, MaxPlayers = 20 };
-        if (joinRandom == true)
-            PhotonNetwork.JoinRandomRoom();
-        else
-            PhotonNetwork.JoinOrCreateRoom(_RoomID, _Options, TypedLobby.Default);
+        PhotonNetwork.JoinRandomRoom();
     }
 
     public override void OnJoinedRoom() {
         PhotonNetwork.LoadLevel(1);
     }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 20 }, null);
+    }
 }
+
