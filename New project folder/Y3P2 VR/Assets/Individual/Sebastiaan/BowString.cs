3fe3823a44e93a711e39809ec1506cd5c0a03e31
Admin <guy_meulen@live.nl>
2019-01-11 12:51:02 +0100

    test

--- New project folder/Y3P2 VR/Assets/Individual/Sebastiaan/BowString.cs
+++ New project folder/Y3P2 VR/Assets/Individual/Sebastiaan/BowString.cs
@@ -88,7 +88,8 @@ public class BowString : Bow
             if (rightHandAxis > 0.85f && currentHand.GetComponent<Controller>().rightHand)
             {
                 firing = true;
-                GameObject _arrow = Instantiate(ammo, transform.position, transform.rotation) as GameObject;
+                GameObject _arrow = PhotonNetwork.Instantiate("Ammo", transform.position, transform.rotation) as GameObject;
+                _arrow.GetPhotonView().TransferOwnership(currentHand.GetPhotonView().Owner);
                 _arrow.transform.SetParent(transform);
                 _arrow.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
                 currentHand.GetComponent<Controller>().item = _arrow.gameObject;
