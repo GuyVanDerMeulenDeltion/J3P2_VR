85531a5dbafc28fba9090a916f73395966f21cbc
GuyvandermeulenDeltion <guy_meulen@live.nl>
2019-01-14 14:36:20 +0100

    Playerbody

--- New project folder/Y3P2 VR/Assets/Individual/Guy/Scripts/PlayerBody.cs
+++ New project folder/Y3P2 VR/Assets/Individual/Guy/Scripts/PlayerBody.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerBody : MonoBehaviour {
 
     private static float rotationTime = 8;
-	
-	private void Update () {
-        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y, transform.eulerAngles.z), rotationTime * Time.deltaTime);
+    [SerializeField]private float yAxisOffset = 0.6f;
+    [SerializeField] private float offset = 0.6f;
+
+    private void Update () {
+        Vector3 _HeadRot = PlayerManager.thisPlayer.player_head.transform.eulerAngles;
+        Vector3 _HeadPos = PlayerManager.thisPlayer.player_head.transform.position;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, _HeadRot.y + 360, transform.eulerAngles.z);
+        transform.position = new Vector3(_HeadPos.x, _HeadPos.y - yAxisOffset, _HeadPos.z);
 	}
 }
