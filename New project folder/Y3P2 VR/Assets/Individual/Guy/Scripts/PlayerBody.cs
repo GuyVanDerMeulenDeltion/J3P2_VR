7da5499cf74240b26daa24871bb2cd19efdc3415
GuyvandermeulenDeltion <guy_meulen@live.nl>
2019-01-14 14:40:14 +0100

    Body test

--- New project folder/Y3P2 VR/Assets/Individual/Guy/Scripts/PlayerBody.cs
+++ New project folder/Y3P2 VR/Assets/Individual/Guy/Scripts/PlayerBody.cs
@@ -7,10 +7,11 @@ public class PlayerBody : MonoBehaviour {
     private static float rotationTime = 8;
     [SerializeField]private float yAxisOffset = 0.6f;
     [SerializeField] private float offset = 0.6f;
+    [SerializeField] private Transform _Head;
 
     private void Update () {
-        Vector3 _HeadRot = PlayerManager.thisPlayer.player_head.transform.eulerAngles;
-        Vector3 _HeadPos = PlayerManager.thisPlayer.player_head.transform.position;
+        Vector3 _HeadRot = _Head.transform.eulerAngles;
+        Vector3 _HeadPos = _Head.transform.position;
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, _HeadRot.y + 360, transform.eulerAngles.z);
         transform.position = new Vector3(_HeadPos.x, _HeadPos.y - yAxisOffset, _HeadPos.z);
 	}
