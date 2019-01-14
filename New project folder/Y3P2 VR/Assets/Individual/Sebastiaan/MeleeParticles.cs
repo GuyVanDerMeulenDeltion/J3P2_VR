dab24a41ec3fa2cd5e70d948b3a03c669aad888c
Admin <guy_meulen@live.nl>
2019-01-14 15:11:29 +0100

    fixing sword particle orientation

--- New project folder/Y3P2 VR/Assets/Individual/Sebastiaan/MeleeParticles.cs
+++ New project folder/Y3P2 VR/Assets/Individual/Sebastiaan/MeleeParticles.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MeleeParticles : MonoBehaviour {
 
     public TrailRenderer trailrenderer { get { return transform.GetComponent<TrailRenderer>(); } }
+
     protected Vector3 oldPos;
     protected Vector3 newPos;
     protected Vector3 customVelocity;
@@ -18,8 +19,6 @@ public class MeleeParticles : MonoBehaviour {
         customVelocity = (oldPos - newPos) / Time.deltaTime;
         oldPos = newPos;
 
-        trailrenderer.widthMultiplier = Mathf.Clamp(customVelocity.magnitude, 0.05f, 1);
-        print(transform.parent.parent);
-        print(customVelocity);
+        trailrenderer.widthMultiplier = Mathf.Clamp(customVelocity.magnitude*20, 1, 20);
 	}
 }
