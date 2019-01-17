29e108d367e3ab11722cf33409efe799ec82eb6d
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2019-01-17 18:00:33 +0100

    Colt_Walker prefab rigged, ready for implement

---
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/ColtWalker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColtWalker : MonoBehaviour {
+
+    public Animator myAnim;
+    private float triggerAxis;
+    public int bulletsLoaded;
+
+    void Update()
+    {
+        triggerAxis = Mathf.Clamp(Input.GetAxis("Horizontal"), 0, 0.95f);
+
+        myAnim.SetFloat("TriggerAxis", triggerAxis);
+        myAnim.SetInteger("Bullets", bulletsLoaded);
+    }
+
+    public void EditBulletcount(int amount)
+    {
+        bulletsLoaded = bulletsLoaded + amount;
+    }
+}
