80f62c607d63eff7dad14e835bc4c3e93aca307f
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2019-01-22 20:16:12 +0100

    Implemented sounds for repeater, ejects casings

---
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/Ejector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejector : MonoBehaviour {
+
+    public Transform ejectionLocation;
+    public GameObject obj;
+
+    public void Eject()
+    {
+        GameObject g = Instantiate(obj, ejectionLocation.position, ejectionLocation.rotation);
+        Destroy(g, 5);
+        g.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up + Vector3.right * Random.Range(7, 15));
+    }
+}
