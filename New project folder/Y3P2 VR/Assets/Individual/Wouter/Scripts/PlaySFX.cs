de137991271e6bd823030cf232ef68b3718e8049
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2019-01-22 19:51:53 +0100

    implemented sounds for colt-walker

---
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Scripts/PlaySFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour
+{
+    public List<AudioSource> audioSourcesSpecific = new List<AudioSource>();
+    public List<AudioSource> audioSourcesRandom = new List<AudioSource>();
+
+    public void PlaySpecificAudio(int whichOne)
+    {
+        audioSourcesSpecific[whichOne].Play();
+    }
+
+    public void PlayRandomAudio()
+    {
+        audioSourcesRandom[Random.Range(0, audioSourcesRandom.Count)].Play();
+    }
+}
