116c7dc268a6866b86af8e800552809969d8ad39
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2019-01-25 13:50:39 +0100

    music for some lvls

---
+++ New project folder/Y3P2 VR/Assets/Individual/Wouter/Sound/LocalMusicController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LocalMusicController : MonoBehaviour {
+
+    private float desiredVolume;
+    public bool iAmCurrentMusic;
+    private AudioSource myMusic;
+    public float lerpTime;
+    [HideInInspector]
+    public bool muffled;
+
+    private void Start()
+    {
+        myMusic = GetComponent<AudioSource>();
+        desiredVolume = myMusic.volume;
+        myMusic.volume = 0;
+
+        if(desiredVolume == 0)
+        {
+            desiredVolume = 1;
+        }
+    }
+
+    private void Update()
+    {
+        if (iAmCurrentMusic)
+        {
+            if (muffled)
+            {
+                myMusic.volume = Mathf.Lerp(myMusic.volume, desiredVolume * 0.5f, 1f / lerpTime * Time.deltaTime);
+            }
+            else
+            {
+                myMusic.volume = Mathf.Lerp(myMusic.volume, desiredVolume * 1, 1f / lerpTime * Time.deltaTime);
+            }
+        }
+        else
+        {
+            myMusic.volume = Mathf.Lerp(myMusic.volume, 0, 1f / lerpTime * Time.deltaTime * 2);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.gameObject.layer == 9)
+            {              
+                iAmCurrentMusic = true;                
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.gameObject.layer == 9)
+            {               
+                iAmCurrentMusic = false;               
+            }
+        }
+    }
+}
